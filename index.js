3df1e7139cd855e32a8d88a948ee2ec87c458938
alidhuniya <alihussaindhuniya0307@gmail.com>
2021-09-23 13:00:16 +0545

    template-engine-pug

--- index.js
+++ index.js
@@ -1,51 +1,12 @@
 const express = require('express')
-var birds = require('./birds')
 const app = express()
 const port = 3000
 
-var myLogger = function (req, res, next) {
-  console.log('LOGGED')
-  next()
-}
-
-app.use(myLogger)
-
-var requestTime = function (req, res, next) {
-  req.requestTime = Date.now()
-  next()
-}
-
-app.use(requestTime)
-
-app.get('/req', function (req, res) {
-  var responseText = 'Hello World!<br>'
-  responseText += '<small>Requested at: ' + req.requestTime + '</small>'
-  res.send(responseText)
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
-
-
-
-app.use('/birds', birds)
-
-app.get('/', (req, res) => {
-  res.send('Hello World!!!')
-})
-app.get('/about', function (req, res) {
-    res.send('about')
-  })
-
-  app.get('/random.txt', function (req, res) {
-    res.send('./random.txt')
-  })
-
-  app.get('/users/:userId/books/:bookId', function (req, res) {
-    res.send(req.params)
-  })
-
-  app.get('/user/:userId(\\d+)', function (req, res) {
-    res.send(req.params)
-  })
+app.set('view engine', 'pug')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
